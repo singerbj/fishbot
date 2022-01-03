105d8de14547c4e430f241bd761889147c7e3ff1
Benjamin Singer <benjmainjsinger@gmail.com>
2022-01-03 17:46:15 -0600

    fix error

--- index.js
+++ index.js
@@ -33,101 +33,101 @@ const getCmdModules = () => {
 };
 
 getCmdModules().done(({ cmdModules, cmdTriggers }) => {
-    process.on('unhandledRejection', function(error) {
-        console.log("####################################################################################################");
-        console.log(error);
-        console.log(error.stack);
+        process.on('unhandledRejection', function(error) {
+            console.log("####################################################################################################");
+            console.log(error);
+            console.log(error.stack);
 
-        client.guilds.cache.forEach((guild) => {
-            let errorChannel;
-            guild.channels.cache.forEach((channel) => {
-                if(channel.name.toLowerCase() === "fishbot-development"){
-                    errorChannel = channel;
+            client.guilds.cache.forEach((guild) => {
+                let errorChannel;
+                guild.channels.cache.forEach((channel) => {
+                    if(channel.name.toLowerCase() === "fishbot-development"){
+                        errorChannel = channel;
+                    }
+                });
+                if(errorChannel){
+                    errorChannel.send(error.message);
+                    errorChannel.send(error.stack);
                 }
             });
-            if(errorChannel){
-                errorChannel.send(error.message);
-                errorChannel.send(error.stack);
-            }
+            console.log("####################################################################################################");
         });
-        console.log("####################################################################################################");
-    });
 
-    client.on("ready", () => {
-        console.log("Fishbot Connected to discord!");
-    });
+        client.on("ready", () => {
+            console.log("Fishbot Connected to discord!");
+        });
 
-    client.on("message", (message) => {
-        if(message.author.id !== client.user.id){
-            let isProfane = false;
-            if(message && message.channel && message.channel.name === "public"){
-                isProfane = filter.isProfane(message.content);
-                if(isProfane){
-                    message.reply("Your manners upset me! Take a moment to think about what you've done.");
-                    message.delete();
+        client.on("message", (message) => {
+            if(message.author.id !== client.user.id){
+                let isProfane = false;
+                if(message && message.channel && message.channel.name === "public"){
+                    isProfane = filter.isProfane(message.content);
+                    if(isProfane){
+                        message.reply("Your manners upset me! Take a moment to think about what you've done.");
+                        message.delete();
+                    }
                 }
-            }
 
-            if(isProfane === false){
-                if(message && message.content && message.content[0] === Util.cmdTriggerSymbol){
-                    allTriggers.forEach(async (trigger) => {
-                        if(message && message.content.trim().split(" ")[0] && message.content.trim().split(" ")[0] === trigger){
-                            console.log("====================================================================================================");
-                            console.log("Command found: " + trigger);
-                            console.log("Message: " + message);
+                if(isProfane === false){
+                    if(message && message.content && message.content[0] === Util.cmdTriggerSymbol){
+                        cmdTriggers.forEach(async (trigger) => {
+                            if(message && message.content.trim().split(" ")[0] && message.content.trim().split(" ")[0] === trigger){
+                                console.log("====================================================================================================");
+                                console.log("Command found: " + trigger);
+                                console.log("Message: " + message);
 
-                            let reply;
-                            if(cmdModules[trigger]){
-                                console.log("Command handler running...");
-                                reply = await cmdModules[trigger].func(message, cmdModules, cmdTriggers, client);
-                            }
-
-                            if(reply){
-                                console.log("Reply: " + reply);
-                                if(typeof reply === "array"){
-                                    reply.forEach((replyPart) => {
-                                        message.reply(replyPart);
-                                    });
-                                } else {
-                                    console.log(reply);
-                                    message.reply(reply);
+                                let reply;
+                                if(cmdModules[trigger]){
+                                    console.log("Command handler running...");
+                                    reply = await cmdModules[trigger].func(message, cmdModules, cmdTriggers, client);
                                 }
-                            } else {
-                                console.log("Reply: (None)");
+
+                                if(reply){
+                                    console.log("Reply: " + reply);
+                                    if(typeof reply === "array"){
+                                        reply.forEach((replyPart) => {
+                                            message.reply(replyPart);
+                                        });
+                                    } else {
+                                        console.log(reply);
+                                        message.reply(reply);
+                                    }
+                                } else {
+                                    console.log("Reply: (None)");
+                                }
+                                console.log("====================================================================================================");
                             }
-                            console.log("====================================================================================================");
-                        }
-                    });
+                        });
+                    }
                 }
             }
-        }
-    });
-    // handle scheduled tasks that run every 250ms
-    setInterval(() => {
-        cmdTriggers.forEach(async (cmdTrigger) => {
-            if(cmdModules[cmdTrigger].runEveryHalfSecond){
-                cmdModules[cmdTrigger].runEveryHalfSecond(client);
-            }
         });
-    }, 500);
+        // handle scheduled tasks that run every 250ms
+        setInterval(() => {
+            cmdTriggers.forEach(async (cmdTrigger) => {
+                if(cmdModules[cmdTrigger].runEveryHalfSecond){
+                    cmdModules[cmdTrigger].runEveryHalfSecond(client);
+                }
+            });
+        }, 500);
 
-    // handle scheduled tasks that run every 10 seconds
-    setInterval(() => {
-        cmdTriggers.forEach(async (cmdTrigger) => {
-            if(cmdModules[cmdTrigger].runEvery10Seconds){
-                cmdModules[cmdTrigger].runEvery10Seconds(client);
-            }
-        });
-    }, 10000);
+        // handle scheduled tasks that run every 10 seconds
+        setInterval(() => {
+            cmdTriggers.forEach(async (cmdTrigger) => {
+                if(cmdModules[cmdTrigger].runEvery10Seconds){
+                    cmdModules[cmdTrigger].runEvery10Seconds(client);
+                }
+            });
+        }, 10000);
 
-    // handle scheduled tasks that run every 60 seconds
-    setInterval(() => {
-        cmdTriggers.forEach(async (cmdTrigger) => {
-            if(cmdModules[cmdTrigger].runEvery60Seconds){
-                cmdModules[cmdTrigger].runEvery60Seconds(client);
-            }
-        });
-    }, 60000);
+        // handle scheduled tasks that run every 60 seconds
+        setInterval(() => {
+            cmdTriggers.forEach(async (cmdTrigger) => {
+                if(cmdModules[cmdTrigger].runEvery60Seconds){
+                    cmdModules[cmdTrigger].runEvery60Seconds(client);
+                }
+            });
+        }, 60000);
 
-    client.login(process.env["discord_key"]);
+        client.login(process.env["discord_key"]);
 });
